9a40106012024575542a50c873c5a669ea4eb36e
Nivanka Fonseka <nivanka@silverstripers.com>
2014-05-29 16:29:11 +0530

    added TOC, with the headers

--- app/parsers/template-parser.inc.php
+++ app/parsers/template-parser.inc.php
@@ -10,7 +10,6 @@ Class TemplateParser {
   }
 
   static function parse($data, $template) {
-
     $template = self::find_template($template);
 
     Twig_Autoloader::register();
@@ -22,8 +21,100 @@ Class TemplateParser {
       'autoescape' => false
     ));
     $twig->addExtension(new Stacey_Twig_Extension());
+	return $twig->render($template, array(
+		'page' 	=> $data,
+		'toc'	=> self::generateTOC()
+	));
+  }
 
-    return $twig->render($template, array('page' => $data));
+  static function generateTOC(){
+	$strFolder = Config::$content_folder;
+
+	$arrItems = self::ReadDir($strFolder, '');
+
+
+	return self::TocHTML($arrItems);
+
+  }
+
+  static function TocHTML($arr, $strClass = ''){
+	  $strHTML = "<ul class='$strClass'>";
+
+	  foreach($arr as $arrItem){
+		$strHTML .= '<li><a href="' . $arrItem['link'] .  '">' . $arrItem['contents']['title'] . '</a>';
+		if(count($arrItem['sub_headings']))
+			$strHTML .= self::TocHTML($arrItem['sub_headings'], 'headings');
+
+		if(count($arrItem['children']))
+		    $strHTML .= self::TocHTML($arrItem['children'], 'headings');
+
+		$strHTML .= '</li>';
+	  }
+
+	  $strHTML .= "</ul>";
+	  return $strHTML;
+  }
+
+  static function ymlContentsFromFolder($strFolder){
+	$strFile = '';
+	foreach(scandir($strFolder) as $file){
+		if(strpos($file, '.yml') !== false){
+			$strFile = $strFolder . '/' . $file;
+		}
+	}
+	if($strFile){
+		return sfYaml::load(file_get_contents($strFile));
+	}
+  }
+
+  static function ReadDir($strFolder = ""){
+	  $arrRet = array();
+      if(!$strFolder){
+		  $strFolder = Config::$content_folder;
+	  }
+	  foreach(scandir($strFolder) as $file){
+		if(in_array($file, array('.', '..')))
+			continue;
+
+		if(preg_match('/^\d.*/', $file) && is_dir($strFolder . '/' . $file)){
+			$arrYML = self::ymlContentsFromFolder($strFolder . '/' . $file);
+			$arrHashes = array();
+
+			$strCurrentURL = Helpers::relative_root_path(
+				Helpers::file_path_to_url($strFolder . '/' . $file));
+
+			if(array_key_exists('content', $arrYML) && !empty($arrYML['content'])){
+				$doc = new DOMDocument();
+				$doc->loadHTML($arrYML['content']);
+
+				foreach(array('h1', 'h2') as $strTagName){
+					$nodeList = $doc->getElementsByTagName($strTagName);
+					foreach ($nodeList as $node) {
+						if($idAttr = $node->getAttribute('id')){
+							$arrHashes[] = array(
+								'link'		=> $strCurrentURL . '#' . $idAttr,
+								'title'		=> $node->nodeValue,
+								'contents'	=> array(
+									'title'	=> $node->nodeValue
+								)
+							);
+						}
+					}
+				}
+			}
+
+			$arr = array(
+				'title'			=> $file,
+				'link'			=> $strCurrentURL,
+				'dir'			=> $strFolder . '/' . $file,
+				'contents'		=> $arrYML,
+				'sub_headings'	=> $arrHashes,
+				'children'		=> self::ReadDir($strFolder . '/' . $file)
+			);
+			$arrRet[] = $arr;
+		}
+	  }
+	  return $arrRet;
   }
 
 }
